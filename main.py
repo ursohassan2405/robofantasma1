db2675994f36fd474065f1658873b0ff3565967a
ursohassan2405 <eliaskaran@gmail.com>
2025-06-21 15:08:53 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
+import time
+import datetime
 
-import websockets
+print("[01] Inicializando robô fantasma...")
 
-print("Conexão com websockets importada com sucesso!")
+ativos = ["NEAR/USDT", "TIA/USDT", "RNDR/USDT"]
+while True:
+    agora = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    for ativo in ativos:
+        print(f"[{agora}] Escaneando ativo: {ativo}")
+        # Simulação de entrada/saída
+        print(f"[{agora}] Nenhum sinal encontrado para {ativo} no momento.")
+    time.sleep(60)
